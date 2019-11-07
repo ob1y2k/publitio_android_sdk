98f13516c84dd4e30255a2591e2fb83a47589c6a
akshay.khandelwal <akshay.khandelwal@galaxyweblinks.in>
2019-11-07 14:21:06 +0530

    Gradle changes

--- app/src/main/java/com/publit/io/MainActivity.java
+++ app/src/main/java/com/publit/io/MainActivity.java
@@ -2,8 +2,8 @@ package com.publit.io;
 
 import android.content.Intent;
 import android.os.Bundle;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
